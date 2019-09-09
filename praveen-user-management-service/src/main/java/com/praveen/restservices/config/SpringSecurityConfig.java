0feb1df1c7d18b34c5a1fd9f1cf3e997c0611a48
Praveen Oruganti <Praveen Oruganti@PraveenOruganti>
2019-09-10 03:40:01 +0530

    updated

--- praveen-user-management-service/src/main/java/com/praveen/restservices/config/SpringSecurityConfig.java
+++ praveen-user-management-service/src/main/java/com/praveen/restservices/config/SpringSecurityConfig.java
@@ -1,27 +1,39 @@
 package com.praveen.restservices.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	DataSource datasource;
 
 	// Authentication based on role
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		       .withUser("praveen")
-		       .password("pcfadmin")
-		       .roles("ADMIN")
-			   .and()
-			   .withUser("prasad")
-			   .password("pcfuser")
-			   .roles("USER");
+//		auth.inMemoryAuthentication()
+//		       .withUser("praveen")
+//		       .password("pcfadmin")
+//		       .roles("ADMIN")
+//			   .and()
+//			   .withUser("prasad")
+//			   .password("pcfuser")
+//			   .roles("USER");
+		
+		auth.jdbcAuthentication()
+		    .dataSource(datasource);		   
 	}
 	
 	@Bean
