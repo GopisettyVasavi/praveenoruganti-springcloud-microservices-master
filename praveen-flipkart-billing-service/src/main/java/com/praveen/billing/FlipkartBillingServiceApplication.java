c3f71771944777a4c7c140d3d674b1529741ee36
Praveen Oruganti <Praveen Oruganti@PraveenOruganti>
2019-09-12 02:49:45 +0530

    updated

--- praveen-flipkart-billing-service/src/main/java/com/praveen/billing/FlipkartBillingServiceApplication.java
+++ praveen-flipkart-billing-service/src/main/java/com/praveen/billing/FlipkartBillingServiceApplication.java
@@ -5,6 +5,7 @@ import static springfox.documentation.builders.RequestHandlerSelectors.basePacka
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -16,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class FlipkartBillingServiceApplication {
 
 	public static void main(String[] args) {
--- praveen-flipkart-ordermanagement-service/src/main/java/com/praveen/ordermanagement/FlipkartOrderManagementServiceApplication.java
+++ praveen-flipkart-ordermanagement-service/src/main/java/com/praveen/ordermanagement/FlipkartOrderManagementServiceApplication.java
@@ -5,8 +5,12 @@ import static springfox.documentation.builders.RequestHandlerSelectors.basePacka
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -17,7 +21,10 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableSwagger2
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class FlipkartOrderManagementServiceApplication {
 
 	public static void main(String[] args) {
@@ -27,7 +34,10 @@ public class FlipkartOrderManagementServiceApplication {
 	@Bean
 	@LoadBalanced
 	   public RestTemplate getRestTemplate() {
-	      return new RestTemplate();
+//		HttpComponentsClientHttpRequestFactory factory =new  HttpComponentsClientHttpRequestFactory();
+//		factory.setConnectTimeout(3000);
+//	      return new RestTemplate(factory);
+		return new RestTemplate();
 	   }
 	
 	@Bean
