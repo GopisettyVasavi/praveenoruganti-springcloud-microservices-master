405803c86e313c521295e88a55cca387752c200d
praveen <praveen@praveen-PC>
2019-10-12 23:42:09 +0530

    updated

--- praveen-user-management-service/src/main/java/com/praveen/restservices/config/SpringSecurityConfig.java
+++ praveen-user-management-service/src/main/java/com/praveen/restservices/config/SpringSecurityConfig.java
@@ -23,14 +23,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	// Authentication based on role
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		       .withUser("praveen")
-		       .password("pcfadmin")
-		       .roles("ADMIN")
-			   .and()
-			   .withUser("prasad")
-			   .password("pcfuser")
-			   .roles("USER");
+//		auth.inMemoryAuthentication()
+//		       .withUser("praveen")
+//		       .password("pcfadmin")
+//		       .roles("ADMIN")
+//			   .and()
+//			   .withUser("prasad")
+//			   .password("pcfuser")
+//			   .roles("USER");
 		
 //		auth.jdbcAuthentication()
 //		    .dataSource(datasource);		   
@@ -44,11 +44,11 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	// Authorization based on role
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			   .antMatchers("/rest/hello/getMsg/admin").hasRole("ADMIN")
-			   .antMatchers("/rest/hello/getMsg/user").hasAnyRole("USER","ADMIN")
-		       .antMatchers("/").permitAll()		       
-		       .and().formLogin();
+//		http.authorizeRequests()
+//			   .antMatchers("/rest/hello/getMsg/admin").hasRole("ADMIN")
+//			   .antMatchers("/rest/hello/getMsg/user").hasAnyRole("USER","ADMIN")
+//		       .antMatchers("/").permitAll()		       
+//		       .and().formLogin();
 	}
 
 }
