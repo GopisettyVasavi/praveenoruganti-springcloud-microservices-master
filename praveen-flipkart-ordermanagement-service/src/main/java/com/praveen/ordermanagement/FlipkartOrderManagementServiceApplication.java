9e4b2e5a74986693f9b7edc2d6a96a6f2ae66bcc
praveen <praveen@praveen-PC>
2019-10-13 23:50:58 +0530

    updated

--- praveen-flipkart-ordermanagement-service/src/main/java/com/praveen/ordermanagement/FlipkartOrderManagementServiceApplication.java
+++ praveen-flipkart-ordermanagement-service/src/main/java/com/praveen/ordermanagement/FlipkartOrderManagementServiceApplication.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import springfox.documentation.builders.ApiInfoBuilder;
