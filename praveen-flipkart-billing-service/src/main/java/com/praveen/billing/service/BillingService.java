d4e5b2e00f5e41e1ef1517ae071cb99662ec54e6
praveen <praveen@praveen-PC>
2019-10-13 16:19:50 +0530

    updated

--- praveen-flipkart-billing-service/src/main/java/com/praveen/billing/service/BillingService.java
+++ praveen-flipkart-billing-service/src/main/java/com/praveen/billing/service/BillingService.java
@@ -9,9 +9,9 @@ public class BillingService {
 	public String billingOrder(@Valid String orderid) throws Exception {
 		int orderidNum=Integer.parseInt(orderid);
 		// This logic is just used for testing the service and will not be used in real time.
-		if(orderidNum>0 && orderidNum<100) {
+		if(orderidNum<=0) {
 			throw new Exception("Invalid Order ID " + orderid);
-		}else if(orderidNum>=100 && orderidNum<500) {
+		}else if(orderidNum>0 && orderidNum<500) {
 			throw new Exception("Payment failed for Order ID "+orderid);
 		}else {
 			return "Successfully placed the order "+orderid;
